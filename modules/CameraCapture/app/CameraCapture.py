c6d9410cb66ebdc2db46ea61c3df3105e3d60f2b
Leif Elgaard Høj <leh@businessmann.dk>
2023-01-15 21:20:29 +0100

    ss

--- modules/CameraCapture/app/CameraCapture.py
+++ modules/CameraCapture/app/CameraCapture.py
@@ -303,7 +303,7 @@ class CameraCapture(object):
         if(predictions.pred_score>threshold):
             try:
                 dimensions = preroi_img_ot.shape
-                cv2.rectangle(preroi_img_ot, (0, 0), (dimensions[0],dimensions[1]), (255,0,0), 2)
+                cv2.rectangle(preroi_img_ot, (dimensions[0],dimensions[1]),(0, 0), (0,0,255), 2)
                 self.__uploadToAzure(filename=rowkey+id,frame=preroi_img)
                 url = "https://camtagstoreaiem.blob.core.windows.net/fiberdefects/"+rowkey+id+ ".jpg"
                 self.ALARM = self.ALARM + 1
