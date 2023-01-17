9cb67071684ffdd676311832ce1a66ba7517ae70
Leif Elgaard Høj <leh@businessmann.dk>
2023-01-17 21:00:46 +0100

    change of color

--- modules/CameraCapture/app/CameraCapture.py
+++ modules/CameraCapture/app/CameraCapture.py
@@ -509,6 +509,7 @@ class CameraCapture(object):
                                     print("created zero")
                             else:
                                 frame1_resized = np.zeros((width,height,3), dtype=np.uint8)
+                                frame1_resized.fill(255)
                                 self.LaneStateUSB1=None
                                 print("USBlane1 not active")
                                 
@@ -530,6 +531,7 @@ class CameraCapture(object):
                                     usberror=usberror+1
                             else:
                                 frame2_resized = np.zeros((width,height,3), dtype=np.uint8)
+                                frame1_resized.fill(255)
                                 self.LaneStateUSB2=None
                                 print("USBlane2 not active")
                         except:
@@ -550,6 +552,7 @@ class CameraCapture(object):
                                     usberror=usberror+1
                             else:
                                 frame3_resized = np.zeros((width,height,3), dtype=np.uint8)
+                                frame3_resized.fill(255)
                                 self.LaneStateUSB3=None
                                 print("USBlane3 not active")
                         except:
@@ -570,6 +573,7 @@ class CameraCapture(object):
                                     usberror=usberror+1
                             else:
                                 frame4_resized = np.zeros((width,height,3), dtype=np.uint8)
+                                frame4_resized.fill(255)
                                 self.LaneStateUSB4=None
                                 print("USBlane4 not active")
 
