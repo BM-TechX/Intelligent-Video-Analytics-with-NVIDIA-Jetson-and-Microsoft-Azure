53ae8f095db418b4722d0b0196abdd109c7b23fc
Leif Elgaard Høj <leh@businessmann.dk>
2023-02-01 14:27:24 +0100

    lanestate

--- modules/CameraCapture/app/CameraCapture.py
+++ modules/CameraCapture/app/CameraCapture.py
@@ -341,7 +341,7 @@ class CameraCapture(object):
     def classify_lane(self,frame,threshold):
         preroi_img_ot,predictions =self.infrencerTopSeondary.getInfrence(frame)
         LaneState = predictions.pred_label + " " + str(round(predictions.pred_score,2))
-        print(predictions)
+        print(LaneState)
         return LaneState
         
     def process_lane(self,frame,threshold,id):
