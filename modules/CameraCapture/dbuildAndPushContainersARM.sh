24c4e0bea225fe553a33fb3ec579713cdfb97183
Leif Elgaard Høj <leh@businessmann.dk>
2023-02-01 23:22:31 +0100

    roleback

--- modules/CameraCapture/dbuildAndPushContainersARM.sh
+++ modules/CameraCapture/dbuildAndPushContainersARM.sh
@@ -1,2 +1,3 @@
+docker stop $(docker ps -a -q --filter ancestor=hubber.azurecr.io/cameracapture:v45 --format="{{.ID}}")
 docker build -t hubber.azurecr.io/cameracapture:v45 -f arm64.Dockerfile .
 docker push hubber.azurecr.io/cameracapture:v45
