c0ef467eaad6a1c420b7a60edd44f0d95720ce78
Leif Elgaard Høj <leh@businessmann.dk>
2023-02-07 08:51:57 +0100

    badfunction

--- modules/CameraCapture/app/findusb.py
+++ modules/CameraCapture/app/findusb.py
@@ -17,11 +17,45 @@ class findUsb:
                         devices[device_path] = serial
                         break
         self.devices = devices
+    def refresh_list(self):
+        output = self.run_v4l2_command("v4l2-ctl --list-devices")
+        lines = output.split('\n')
+        devices = {}
+        for line in lines:
+            parts = line.split()
+            if "video" in line:
+                device_path = line.strip()
+                device_info = self.run_v4l2_command(f"v4l2-ctl --device={device_path} --info")
+                for info_line in device_info.split('\n'):
+                    if 'Serial' in info_line:
+                        serial = info_line.split(':')[1].strip()
+                        devices[device_path] = serial
+                        print(serial)
+                        break
+        self.devices = devices
     def run_v4l2_command(self,command):
         result = subprocess.run(command.split(), stdout=subprocess.PIPE)
         return result.stdout.decode('utf-8').strip()
     def getDevice(self,serial):
+        
+        output = self.run_v4l2_command("v4l2-ctl --list-devices")
+        lines = output.split('\n')
+        devices = {}
+        for line in lines:
+            parts = line.split()
+            if "video" in line:
+                device_path = line.strip()
+                device_info = self.run_v4l2_command(f"v4l2-ctl --device={device_path} --info")
+                for info_line in device_info.split('\n'):
+                    if 'Serial' in info_line:
+                        serial = info_line.split(':')[1].strip()
+                        devices[device_path] = serial
+                        print(serial)
+                        break
+        self.devices = devices
+        
         selected_device = None
+        print("Looking for device with serial: " + serial)
         for device_path, serial in self.devices.items():
             if serial == desired_serial:
                 selected_device = device_path
